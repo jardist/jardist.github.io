2fb793de474807e05eecb3ef856d0441a29074f8
JarDist <67625063+jardist@users.noreply.github.com>
2026-09-08 00:50:29 +0700

    Create anchorAd.js

---
+++ terasjagat/assets/js/anchorAd.js
@@ -0,0 +1,79 @@
+(function () {
+  const ANCHOR_SELECTOR =
+    'ins.adsbygoogle[data-anchor-status="displayed"]';
+  function fixAnchorAd(element) {
+    if (!element.matches(ANCHOR_SELECTOR)) {
+      return;
+    }
+    element.style.setProperty(
+      'margin-bottom',
+      '80px',
+      'important'
+    );
+  }
+  function observeAnchorAd(element) {
+    if (!element || element.dataset.anchorObserver === 'true') {
+      return;
+    }
+    element.dataset.anchorObserver = 'true';
+    fixAnchorAd(element);
+    const anchorObserver = new MutationObserver(function (mutations) {
+      mutations.forEach(function (mutation) {
+        if (mutation.type === 'attributes') {
+          if (
+            mutation.attributeName === 'style' ||
+            mutation.attributeName === 'data-anchor-status'
+          ) {
+            if (element.matches(ANCHOR_SELECTOR)) {
+              fixAnchorAd(element);
+            }
+          }
+        }
+      });
+    });
+    anchorObserver.observe(element, {
+      attributes: true,
+      attributeFilter: [
+        'style',
+        'data-anchor-status'
+      ]
+    });
+  }
+  function scanAnchorAds() {
+    document
+      .querySelectorAll(ANCHOR_SELECTOR)
+      .forEach(function (element) {
+        observeAnchorAd(element);
+      });
+  }
+  scanAnchorAds();
+  const documentObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.type !== 'childList') {
+        return;
+      }
+      mutation.addedNodes.forEach(function (node) {
+        if (node.nodeType !== 1) {
+          return;
+        }
+        if (
+          node.matches &&
+          node.matches(ANCHOR_SELECTOR)
+        ) {
+          observeAnchorAd(node);
+        }
+        if (node.querySelectorAll) {
+          node
+            .querySelectorAll(ANCHOR_SELECTOR)
+            .forEach(function (element) {
+              observeAnchorAd(element);
+            });
+        }
+      });
+    });
+  });
+  documentObserver.observe(document.documentElement, {
+    childList: true,
+    subtree: true
+  });
+})();
